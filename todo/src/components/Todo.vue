1f23afcc19a334bedde0f2e2937934b87479c5bd
byeongjeon jeon <pensieve92@gmail.com>
2022-01-05 21:05:42 +0900

    complete Todo App

---
+++ todo/src/components/Todo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="mb-2 d-flex">
+    <div>
+      <input
+          type="checkbox"
+          :checked="todo.checked"
+          @change="toggleCheckbox"
+      />
+    </div>
+    <span
+        class="ml-3 flex-grow-1"
+        :class="todo.checked ? 'text-muted' : ''"
+        :style="todo.checked ? 'text-decoration: line-through' : '' "
+    >
+      {{ todo.text }}
+    </span>
+    <button
+        class="btn btn-danger btn-sm"
+        @click="clickDelete"
+    >
+      Delte
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Todo',
+  props:{
+    todo: {
+      type: Object,
+      require: true
+    }
+  },
+  methods:{
+    toggleCheckbox(e){
+      this.$emit('toggle-checkbox',{
+        id: this.todo.id,
+        checked: e.target.checked
+      })
+    },
+    clickDelete(){
+      this.$emit('click-delete', this.todo.id)
+    }
+  }
+}
+</script>
