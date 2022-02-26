e05f75337f3660b661cdf5dd8b819aa3d799a01e
byeongjeon jeon <pensieve92@gmail.com>
2022-02-26 21:30:56 +0900

    aysnc await 테스트

--- todo/src/store/modules/user.js
+++ todo/src/store/modules/user.js
@@ -2,19 +2,30 @@ import axios from "axios";
 
 export default {
     namespaced: true,
-    state: { //data
+    state     : { //data
         users: []
     },
-    mutations: { //state 변경
-        SET_USERS(state, users){
+    mutations : { //state 변경
+        SET_USERS(state, users) {
             state.users = users
         },
     },
-    actions: { //methods
-        getUsers({commit}){
-            axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
-                commit('SET_USERS', res.data)
-            });
+    actions   : { //
+        // await이 붙은 경우: start -> finally -> end
+        // await이 없는 경우: start -> end -> finally
+        async getUsers({commit}) {
+            console.log('start');
+            await axios.get('https://jsonplaceholder.typicode.com/users')
+                .then(res => {
+                    commit('SET_USERS', res.data)
+                })
+                .catch(err => {
+                    console.log(err);
+                }).finally(function () {
+                    console.log('finally');
+                }
+            );
+            console.log('end');
         },
 
     },
